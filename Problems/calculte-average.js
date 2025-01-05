cb95e9a1abe79c8aa7fe824899c928c85d18da0d
ahmadabdallahh <ahmadabhossen@gmail.com>
2025-01-05 19:14:13 +0200

    `Removed elzero problem.js and updated index.html to include calculte-average.js instead of conver-number-into-string.js.`

---
+++ Problems/calculte-average.js
@@ -0,0 +1,15 @@
+/*
+    - Write a function which calculates the average of the numbers in a given array.
+    - Note: Empty arrays should return 0.
+*/
+
+function findAverage(array) {
+    // let sum = 0;
+    // for (let i = 0; i < array.length; i++) {
+    //     sum += array[i];
+    // }
+    // return sum / array.length;
+    return array.map((x) => x + x);
+}
+
+console.log(findAverage([1, 2, 3]));
--- elzero problem.js
+++
@@ -1,5 +0,0 @@
-let array = [3, 7, 3, 5, 11, 9, 7, 1];
-
-let oddNum = array.filter((x) => (x % 2 !== 0 ? x : ''));
-
-console.log(oddNum);
