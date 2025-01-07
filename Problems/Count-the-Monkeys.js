96eb69a5b737f3e7e6a9c834a9112250d5614502
ahmadabdallahh <ahmadabhossen@gmail.com>
2025-01-07 21:19:03 +0200

    `Update index.html to load Count-the-Monkeys.js instead of convert-a-string-to-a-Number.js`

---
+++ Problems/Count-the-Monkeys.js
@@ -0,0 +1,26 @@
+/* Count the Monkeys! */
+
+/*
+    You take your son to the forest to see the monkeys. You know that there are a certain
+    number there (n), but your son is too young to just appreciate the full number, he has to start 
+    counting them from 1.
+    As a good parent, you will sit and count with him. Given the number (n), populate an 
+    array with all numbers up to and including that number, but excluding zero.
+*/
+
+/* Solution */
+
+// Create Empty Array
+// Loop through number given
+// Push the number to Empty Array
+// Return the total Value
+
+function monkeyCount(n) {
+    let newArray = [];
+    for (let i = 1; i <= n; i++) {
+        newArray.push(i);
+    }
+    return newArray;
+}
+
+console.log(monkeyCount(20));
